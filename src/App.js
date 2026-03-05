4c4d0c4fd93e8cca8bc5b8c11187ec020fd674f3
wcspinella <wcspinella@gmail.com>
2026-03-05 13:26:28 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // 1. A list (array) of restaurant names
+  const restaurantList = ["Bamboo", "King Curry", "Brooklyn Carreta", "Chipotle"];
+  // 2. Some state to store the "picked" restaurant (hint: useState)
+  const [selectedRestaurant, setSelectedRestaurant] = useState("");
+
+  // 3. A function that picks a random one from the list
+  function selectRestaurant() {
+  const randomIndex = Math.floor(Math.random() * restaurantList.length);
+  setSelectedRestaurant(restaurantList[randomIndex]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // 4. Return some JSX with:
+  //    - a button that triggers the function
+  //    - something to display the result
+  <div className = "main-container">
+  <button onClick = {selectRestaurant}>Let's Eat</button>
+  <h1>Here's where you're eating:</h1>
+  <p>{selectedRestaurant}</p>
+  </div>
   );
 }
 
