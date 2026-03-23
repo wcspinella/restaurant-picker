15d6156f00f33252c314c60148672b480d7cf1e3
wcspinella <wcspinella@gmail.com>
2026-03-23 16:53:44 -0700

    Added new places

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import './App.css';
 
 function App() {
   // 1. A list (array) of restaurant names
-  const lowPriceList = ["Chipotle", "Franks-A-Lot", "Panda Express", "Taco Bell"]
+  const lowPriceList = ["Burgerville", "Chipotle", "Egyptian Bros", "Franks-A-Lot", "Grand Central Bakery", "Hong Kong 97", 
+    "Jersey Mike's", "The Meddling Lime", "Midpoint", "Noodles And Co.", "Panda Express",  "Panera Bread", "Pizzicato", 
+    "Popeyes", "Taco Bell"]
 
-  const midPriceList = ["Afuri", "Ate-Oh-Ate", "Baby Doll Pizza", "Bar Bar", "Boke Bowl", "Botto's BBQ", 
-    "Brooklyn Carreta", "Bugerville", "Chungdam", "Crisp", "Dove Vivi", "Egyptian Bros", "Fire On The Mountain", 
-    "FOMO Chicken", "Fresh Fish Poke", "Grassa", "Grand Central Bakery", "Grand Fir Brwery","GrindWitTryz", "Guero",
-    "Hat Yai", "Hawthorne Asylum", "The Heist", "Hog Wild BBQ", "Hong Kong 97", "J & R Mexican Food", "Jersey Mike's", 
-    "Ken's Artisan Pizza", "Khao Moo Dang", "Killer Burger", "King Curry", "Lardo","Matador", "Loyal Legion", 
-    "Matt's BBQ Tacos", "Milwaukie Cafe", "Mike's Drive In", "MOD Pizza", "Montelupo's Foccaceria", "MX21 Mexican", 
-    "Noodles And Co.", "The Office", "Panera Bread", "Pfriem", "Pizzicato", "Pop Pizza", "Por Que No", "Popeyes", "Red Robin", 
-    "Sebastiano's", "Schilling Cider Co.", "Sizzlin Indian Grill", "Slide Inn Tavern", "Stammtisch", "SteepleJack", "Toji",
-    "Wrap And Roll", "Ya Hala", "Yang's Noodle", "The Zed"]
+  const midPriceList = ["Afuri", "Akira Sushi", "Ate-Oh-Ate", "Baby Doll Pizza", "Bai Mint Thai", "Bar Bar", "Bastion", 
+    "Bluto's", "Boke Bowl", "Bollywood Theater", "Botto's BBQ", "Brooklyn Carreta", "Chungdam", "Cricket Cafe", "Crisp", 
+    "Dove Vivi", "East Side Delicatessen", "EEM", "Enat Kitchen", "Ex Novo Brewing", "Fantino", "Fire On The Mountain", 
+    "FOMO Chicken", "Fresh Fish Poke", "Gilda's", "Grassa", "Grand Fir Brewing","GrindWitTryz", "Güero", "Hat Yai", 
+    "Hawthorne Asylum", "The Heist", "Hog Wild BBQ", "J & R Mexican Food", "Jade Bistro", "Jam on Hawthorne", 
+    "Ken's Artisan Pizza", "Khao Moo Dang", "Killer Burger", "King Curry", "La Bonita", "Lardo", "Laughing Planet", 
+    "Lovely's Fifty Fifty", "Loyal Legion", "Maruti", "Matador", "Matt's BBQ Tacos", "MidCity SmashedBurger", 
+    "Milwaukie Cafe", "Mike's Drive In", "MOD Pizza", "Montelupo's Foccaceria", "MX21 Mexican", "Monty's Red Sauce", 
+    "Moon Pocha", "The Original Halibuts", "Paadee", "Pfriem", "Podnah's Pit BBQ", "Pop Pizza", "Por Que No", 
+    "Pretty Boy Pizza", "Queen of Sheba", "Red Robin", "Sebastiano's", "Schilling Cider Co.", "Shalom Y'all", 
+    "Sizzlin Indian Grill", "Slide Inn Tavern", "Stammtisch", "SteepleJack", "Sweedeedee", "Tabor Bread", "Toji", 
+    "Vida 33", "Wrap And Roll", "Ya Hala", "Yalla", "Yang's Noodle", "The Zed"]
 
-  const highPriceList = ["Akira Sushi", "Bamboo Sushi SE", "EEM", "Gilda's", "Han Oak", "Maruti",
-  "Monty's Red Sauce", "Moon Pocha", "Vida 33"]
+  const highPriceList = ["Bamboo Sushi", "Bluefin Sushi", "Dolly Olive", "Han Oak"]
 
   // 2. Some state to store the "picked" restaurant (hint: useState)
   const [selectedRestaurant, setSelectedRestaurant] = useState("");
